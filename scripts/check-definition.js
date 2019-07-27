8ee73f9b37f7c15070274c650d75e651be3ffc84
jamieg <jamie.garner@caplin.com>
2019-07-27 22:24:52 +0100

    initial commit

---
+++ scripts/check-definition.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const readFile = promisify(fs.readFile);
+
+const dictionaryPath = path.resolve(__dirname, "../data/all-words.txt");
+
+const isWordDefined = async word => {
+  try {
+    const data = await readFile(dictionaryPath, "utf8");
+    const regex = new RegExp(`${word.toLowerCase()}\r`);
+    const match = regex.exec(data);
+    // if (match) {
+    //   console.log(">>> match: ", match[0]);
+    // } else {
+    //   console.log(">>> no match");
+    // }
+
+    return !!match;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// isWordDefined("aahaf")
+
+module.exports = isWordDefined;
--- scripts/rhymes.js
+++ scripts/rhymes.js
@@ -1,21 +1,39 @@
 const fetch = require("node-fetch");
-const words = require("../temp-data/words");
-
+const words = require("../data/common-words");
+const isWordDefined = require("./check-definition");
 
 // - call https://www.datamuse.com/api/ (e.g. https://api.datamuse.com/words?rel_rhy=list)
 // - call API with each word, sort in order of most number of rhyming words
 // - split into easy, medium, hard groups
 
-const url = `https://api.datamuse.com/words?rel_rhy=${words[0]}`;
+// const url = `https://api.datamuse.com/words?rel_rhy=${words[0]}`;
+const url = `https://api.datamuse.com/words?rel_rhy=list`;
+
+const filterWordsWithSpaces = words => {
+  return words.filter(result => !result.word.match(/ /g));
+};
+
+const filterUndefinedWords = async words => {
+  const promises = await words.map(result => isWordDefined(result.word));
+  const definedWordMap = await Promise.all(promises);
+
+  return words.filter((result, index) => definedWordMap[index]);
+};
 
 const getRhymes = async url => {
   try {
     const response = await fetch(url);
     const json = await response.json();
-    console.log(json);
+    const spacesFiltered = filterWordsWithSpaces(json);
+
+    const definedWords = await filterUndefinedWords(spacesFiltered);
+    console.log("defined: ", definedWords);
+
+    // TODO: Store in my db
   } catch (error) {
     console.log(error);
   }
 };
 
+console.log("Words that Rhyme with ", words[0]);
 getRhymes(url);
