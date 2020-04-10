8cd442d386704826a9e7117617e190b500463af6
Jamie Garner <jamiegarner123@gmail.com>
2020-04-10 08:18:19 +0800

    tweaks

--- scripts/rhymes/restore-from-backup.js
+++ scripts/rhymes/restore-from-backup.js
@@ -2,7 +2,31 @@ const MongoClient = require("mongodb").MongoClient;
 const uri = require("../../uri");
 const rhymesData = require("../../backup/rhymes-backup");
 
-const insertRhymesIntoDbFirstTime = rhymes => {
+// const insertRhymesIntoDbFirstTime = rhymes => {
+//   console.log(">>> Connecting to db");
+
+//   MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+//     if (err) throw err;
+
+//     console.log(">>> Connected");
+//     const db = client.db("words");
+
+//     db.collection("rhymes").drop(err => {
+//       if (err) throw err;
+
+//       console.log("Collection Dropped");
+
+//       db.collection("rhymes").insertMany(rhymes, function(err, res) {
+//         if (err) throw err;
+
+//         console.log("Number of documents inserted: " + res.insertedCount);
+//         client.close();
+//       });
+//     });
+//   });
+// };
+
+const insertRhymesIntoDb = rhymes => {
   console.log(">>> Connecting to db");
 
   MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
@@ -11,38 +35,13 @@ const insertRhymesIntoDbFirstTime = rhymes => {
     console.log(">>> Connected");
     const db = client.db("words");
 
-    db.collection("rhymes").drop(err => {
+    db.collection("rhymes").insertMany(rhymes, function(err, res) {
       if (err) throw err;
 
-      console.log("Collection Dropped");
-
-      db.collection("rhymes").insertMany(rhymes, function(err, res) {
-        if (err) throw err;
-
-        console.log("Number of documents inserted: " + res.insertedCount);
-        client.close();
-      });
+      console.log("Number of documents inserted: " + res.insertedCount);
+      client.close();
     });
   });
 };
 
-// const insertRhymesIntoDb = rhymes => {
-//   console.log(">>> Connecting to db");
-//   console.log(">>> RhymesData: ", rhymesData[0]);
-
-//   MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
-//     if (err) throw err;
-
-//     console.log(">>> Connected");
-//     const db = client.db("words");
-
-//     db.collection("rhymes").insertMany([rhymesData[0]], function(err, res) {
-//       if (err) throw err;
-
-//       console.log("Number of documents inserted: " + res.insertedCount);
-//       client.close();
-//     });
-//   });
-// };
-
-// insertRhymesIntoDbFirstTime(rhymesData);
+insertRhymesIntoDb(rhymesData);
