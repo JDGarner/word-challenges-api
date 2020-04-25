56053d15840e615cfc31abfafbeaed631cfbdd18
Jamie Garner <jamiegarner123@gmail.com>
2020-04-25 11:50:52 +0800

    revert constants

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: "3000",
-  DB: "words_dev",
-  DB_URI: "mongodb://localhost:27017/words_dev"
+  PORT: "3001",
+  DB: "words_prod",
+  DB_URI: "mongodb://localhost:27017/words_prod"
 };
