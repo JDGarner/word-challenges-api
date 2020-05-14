f72fd2e36a57d984f4686784c9aad7bbea7ed9f7
Jamie Garner <jamiegarner123@gmail.com>
2020-05-14 15:31:21 +0800

    migration stuff

--- scripts/synonyms/get-words.js
+++ scripts/synonyms/get-words.js
@@ -4,24 +4,30 @@ const { DB_URI, DB } = require("../../constants");
 
 const uri = DB_URI;
 
-MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
-  if (err) throw err;
+const getWords = (difficulty) => {
+  MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
+    if (err) throw err;
 
-  console.log(">>> Connected");
-  const db = client.db(DB);
+    console.log(">>> Connected");
+    const db = client.db(DB);
 
-  db.collection("definitions")
-    .find({ difficulty: "novice" })
-    .toArray((err, results) => {
-      if (err) throw err;
-
-      const fileContent = results.map((r) => r.word).join(">>> \n");
-
-      fs.writeFile(`scripts/synonyms/novice`, fileContent, function (err) {
+    db.collection("definitions")
+      .find({ difficulty })
+      .toArray((err, results) => {
         if (err) throw err;
-        console.log("Success!");
-      });
 
-      client.close();
-    });
-});
+        const fileContent = results.map((r) => r.word).join(">>> \n");
+
+        fs.writeFile(`scripts/synonyms/${difficulty}`, fileContent, function (err) {
+          if (err) throw err;
+          console.log("Success!");
+        });
+
+        client.close();
+      });
+  });
+};
+
+getWords("journeyman");
+getWords("expert");
+getWords("master");
