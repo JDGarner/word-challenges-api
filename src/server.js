a0340f72156f4bf47d3f645cdbc2173f1c78a39b
Jamie Garner <jamiegarner123@gmail.com>
2020-09-06 20:26:01 +0200

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -11,22 +12,25 @@ const startServer = (rootPath, port, dbUri) => {
   app.use(BodyParser.json());
   app.use(BodyParser.urlencoded({ extended: true }));
 
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
   });
 
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
     if (req.headers.authorization !== process.env.WORDS_API_KEY) {
-      return res.status(403).json({ error: '403 Forbidden' });
+      return res.status(403).json({ error: "403 Forbidden" });
     }
     next();
   });
 
   app.use(rootPath, routes);
 
-  app.listen(port, function(error) {
+  app.listen(port, function (error) {
     if (error) throw error;
     console.log("Server running on PORT " + port);
   });
